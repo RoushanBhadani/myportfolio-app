562c64105f169e6ba55c9d72c2ced5ac1c712413
RoushanWebpristine <roushanwebpristine@gmail.com>
2025-10-03 03:47:10 +0530

    integrate name animation

--- screens/home/HomeContent.jsx
+++ screens/home/HomeContent.jsx
@@ -1,105 +1,97 @@
-import { Button, Image, StyleSheet, Text, View } from 'react-native'
-import Animated, {
-  useSharedValue,
-  withTiming,
-  useAnimatedStyle,
-  Easing,
-} from 'react-native-reanimated';
+import { useEffect } from "react";
+import { Image, StyleSheet, Text, View } from "react-native";
+import Animated, { useSharedValue, useAnimatedStyle, withRepeat, withTiming } from "react-native-reanimated";
 
 function HomeContent() {
-  const randomWidth = useSharedValue(10);
+  const spacing = useSharedValue(0);
 
-  const config = {
-    duration: 500,
-    easing: Easing.bezier(0.5, 0.01, 0, 1),
-  };
+  useEffect(() => {
+    spacing.value = withRepeat(
+      withTiming(10, { duration: 1800 }),
+      -1,
+      true
+    );
+    
+  }, []);
 
-  const style = useAnimatedStyle(() => {
+  const animatedStyle = useAnimatedStyle(() => {
     return {
-      width: withTiming(randomWidth.value, config),
+      letterSpacing: spacing.value,
+      textShadowRadius: spacing.value+30,
     };
   });
+
   return (
     <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: 30,
+      }}
+    >
+      <Text style={{ color: "white", fontSize: 28, fontWeight: "600" }}>
+        Hi I'm
+      </Text>
+
+      <Animated.Text style={[styles.name, animatedStyle, { textShadowColor: "#f57b00ff" }]}>
+        ROUSHAN
+      </Animated.Text>
+
+      <Text
         style={{
-          flex: 1,
-          justifyContent: "center",
-          alignItems: "center",
-          marginTop: 30,
+          color: "#ffffff",
+          fontSize: 28,
+          fontWeight: "600",
+          textAlign: "center",
+          marginTop: 12,
         }}
       >
+        Transforming Ideas into Digital Reality
+      </Text>
 
-        <Animated.View style={[styles.box, style]} />
-      <Button
-        title="toggle"
-        onPress={() => {
-          randomWidth.value = Math.random() * 350;
+      <Text
+        style={{
+          color: "#ffffff",
+          fontSize: 15,
+          fontWeight: "600",
+          textAlign: "center",
+          marginTop: 12,
         }}
-      />
-        <Text style={{ color: "white", fontSize: 28, fontWeight: 600 }}>
-          Hi I'm
-        </Text>
-        <Text style={{ color: "#f57b00ff", fontSize: 48, fontWeight: 600 }}>
-          Roushan
-        </Text>
-        <Text
-          style={{
-            color: "#ffffff",
-            fontSize: 28,
-            fontWeight: 600,
-            textAlign: "center",
-            marginTop: 12,
-          }}
-        >
-          Transforming Ideas into Digital Reality
-        </Text>
+      >
+        Contact Number: <Text style={{ color: "#f57900" }}>7484030370</Text>
+      </Text>
 
-        <Text
-          style={{
-            color: "#ffffff",
-            fontSize: 20,
-            fontWeight: 600,
-            textAlign: "center",
-            marginTop: 12,
-          }}
-        >
-          Contact Number: <Text style={{ color: "#f57b00ff" }}>7484030370</Text>
-        </Text>
-        <Text
-          style={{
-            color: "#ffffff",
-            fontSize: 16,
-            fontWeight: 600,
-            textAlign: "center",
-            marginTop: 12,
-          }}
-        >
-          Innovative Frontend Creator | Building Sleek and User-Friendly Web
-          Applications
-        </Text>
-        <View>
-          <Image
-            source={require("../../assets/boss.png")}
-            style={{ width: 280, height: 400 }}
-            resizeMode="contain"
-          />
-        </View>
+      <Text
+        style={{
+          color: "#f57900",
+          fontSize: 16,
+          fontWeight: "400",
+          textAlign: "center",
+          marginTop: 12,
+        }}
+      >
+        Innovative Frontend Creator | Building Sleek and User-Friendly Web
+        Applications
+      </Text>
+
+      <View>
+        <Image
+          source={require("../../assets/boss.png")}
+          style={{ width: 280, height: 400 }}
+          resizeMode="contain"
+        />
       </View>
-  )
+    </View>
+  );
 }
 
-export default HomeContent
+export default HomeContent;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  box: {
-    width: 100,
-    height: 80,
-    backgroundColor: 'black',
-    margin: 30,
+  name: {
+    color: "#f57b00ff",
+    fontSize: 48,
+    fontWeight: "600",
   },
 });
